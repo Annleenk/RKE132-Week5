7ded10c9947496b06f5a188a5711848f72064ec7
ann 🌼 <annleenk@gmail.com>
2024-01-07 19:26:03 +0200

    add week5

---
+++ Week5ANDOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿// range math >= 90, biology >=90, chemistry >=90
+
+using System.ComponentModel.Design;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter yoru chemistry result.");
+chemistry = Int32.Parse(Console.ReadLine());
+
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congrats, you've been accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved");
+}
---
+++ Week5ANDRanges/Program.cs
@@ -0,0 +1,31 @@
+﻿//temp <= 0 - freezing cold
+//temp > AND temp <= - cold
+//temp >10 AND temp <= 15 - chilly 10-15
+//temp >15 AND <20 - warm 15-20
+//temp >=20 AND temp <30 - hot 20-30
+//temp >=30 - boiling hot
+
+
+Console.WriteLine("Insert the temperature");
+int temp = Int32.Parse(Console.ReadLine());
+
+if(temp >= 30)
+{
+    Console.WriteLine("Boiling hot");
+}
+else if(temp <30 && temp >=20 )
+{
+    Console.WriteLine("Hot");
+}
+else if(temp <20 && temp >= 15)
+{
+    Console.WriteLine("warm");
+}
+else if (temp <15 && temp >= 10)
+{
+    Console.WriteLine("chilly");
+}
+else if (temp < 10)
+{
+    Console.WriteLine("freezing cold");
+}
